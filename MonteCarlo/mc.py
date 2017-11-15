d301f41a12b2fdc8418eb5910dc80e6d4d745d3a
Jader Martins <jadermcs94@gmail.com>
2017-11-15 16:55:02 -0200

    correct log file

--- MonteCarlo/mc.py
+++ MonteCarlo/mc.py
@@ -9,16 +9,17 @@ from pyspark import SparkContext
 if __name__ == "__main__":
     NUM_SAMPLES = 100000000
     sc = SparkContext()
-    with open("gs://spark-exp/montecarlo-py.log","w") as log:
-        for _ in xrange(100):
-            t0 = time.time()
+    with open("montecarlo-py.log","w") as log:
+        for _ in range(10):
 
             def inside(p):
                 x, y = random.random(), random.random()
                 return x*x + y*y < 1
 
-            count = sc.parallelize(xrange(0, NUM_SAMPLES)) \
+            t0 = time.time()
+
+            count = sc.parallelize(range(0, NUM_SAMPLES)) \
                 .filter(inside).count()
-            print("Elapsed time: %f\n" % (time.time()-t0), file=log)
-            print("Pi is roughly %f\n" % (4.0 * count / NUM_SAMPLES), \
+            print("Elapsed time: %f" % (time.time()-t0), file=log)
+            print("Pi is roughly: %f" % (4.0*count / NUM_SAMPLES), \
                     file=log)
